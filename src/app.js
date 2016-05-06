a16cd983de9aadf6d2e1f9cce271958b6027be29
David Kim <dhkim1211@gmail.com>
2016-05-06 11:46:32 +0200

    takes parameter for folder definition

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 //Import file system functionality
 var fs = require('fs');
 
-fs.readdir('resources', function(err, data){
+var folderToRead = process.argv[2];
+
+if (folderToRead == undefined) {
+	console.log("Please specify a folder");
+	return;
+}
+
+fs.readdir(folderToRead, function(err, data){
 	if (err) throw err; //catch error
 	console.log(data);
 	data.forEach(function(file){ //loop through the file array
-		fs.readFile('resources/' + file, 'utf8', function(err, filedata){ //read each file in array
+		fs.readFile(folderToRead + "/" + file, 'utf8', function(err, filedata){ //read each file in array
 			if (err) {
 			throw err;
 			} //catch error
-			filedata = JSON.parse(filedata); 
-			console.log(filedata);
+			console.log(JSON.parse(filedata));
 		})
 	})
 });
